4f949dc794820edf0f3c5bce836a45a0ef6724e3
litoralcar <criacao1@litoralcar.com.br>
2022-06-15 17:34:53 -0300

    Novas alterações

--- home.php
+++ home.php
@@ -1,7 +1,10 @@
 <?php
   session_start();
 
-  echo $_SESSION['autenticado'];
+  if(!isset($_SESSION['autenticado']) || $_SESSION['autenticado']){
+    header('Location: index.php?login=erro2');
+  }
+
 ?>
 <html>
   <head>
